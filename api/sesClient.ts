0d66905cdecf15339ae7a90e1e65ae2444af516f
Nayla Pereira <naylapereira06@gmail.com>
2026-08-25 10:20:24 -0300

    Ajuste configuracion local de AWS SES

--- api/sesClient.ts
+++ api/sesClient.ts
@@ -1,7 +1,7 @@
 import { SESClient } from "@aws-sdk/client-ses";
 
 export const sesClient = new SESClient({
-  region: process.env.AWS_REGION,
+  region: process.env.AWS_REGION ?? "us-east-2",
   credentials: {
     accessKeyId: process.env.AWS_ACCESS_KEY_ID ?? "",
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY ?? "",
